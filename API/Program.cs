1bdb7fc18920d62be7a9d6af4a63ec8e7b7987fb
AzureAD\TosheePandey <toshee.pandey@health.telstra.com>
2024-10-08 23:06:30 +1100

    React project Added

--- API/Program.cs
+++ API/Program.cs
@@ -12,6 +12,16 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors(opt => {
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader()
+            .AllowAnyMethod()
+            .WithOrigins("http://localhost:3000");
+    });
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,6 +30,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
+app.UseAuthorization();
 
 using var scope = app.Services.CreateScope();
 
